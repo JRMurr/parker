e0a029462ede260bddeaeeaf63ddf065249ad8e5
Lucas Pickering <lucaspickering9@gmail.com>
2018-09-30 11:26:28 -0400

    Slightly improve POST script

--- post.py
+++ post.py
@@ -5,12 +5,18 @@ import requests
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('route')
+    parser.add_argument('route', help="Route to store the doc under")
+    parser.add_argument('--url', '-u', default='http://localhost:8000', help="Server URL")
     parser.add_argument('--template', '-t', default='page')
-    parser.add_argument('--content', '-c', default='')
+    parser.add_argument('--content', '-c', required=True)
     args = parser.parse_args()
-    argd = vars(args)
 
-    resp = requests.post(f'http://localhost:8000/', json=argd)
+    with open(args.content) as f:
+        content = f.read()
+    resp = requests.post(args.url, json={
+        'route': args.route,
+        'template': args.template,
+        'content': content
+    })
     print(resp.status_code)
     print(resp.text)
